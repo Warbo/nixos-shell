2d3bdde2ca580f1f6e66c682ab6da00bcf6cd69c
Chris Warburton <chriswarbo@gmail.com>
2025-03-06 21:42:54 +0000

    Parameterise pkgs set
    Parameterising the nixpkgs path is better than hard-coding, but parameterising
    the nixpkgs attrset itself is even better (e.g. to use different versions of
    nixpkgs in different places, to fiddle with the config and overlays, etc.).

--- default.nix
+++ default.nix
@@ -1,10 +1,9 @@
 {
-  nixpkgs ? <nixpkgs>, system ? builtins.currentSystem
+  pkgs ? import nixpkgs { inherit system; },
+  nixpkgs ? <nixpkgs>,
+  system ? builtins.currentSystem
 }:
-
-with import nixpkgs { inherit system; };
-
-buildGoModule {
+pkgs.buildGoModule {
   name = "nixos-shell";
   src = ./.;
   vendorSha256 = "0gjj1zn29vyx704y91g77zrs770y2rakksnn9dhg8r6na94njh5a";
