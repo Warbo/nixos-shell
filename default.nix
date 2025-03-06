d9c7cf4ee1b595d07355abe5d407930d2e0510ae
Chris Warburton <chriswarbo@gmail.com>
2025-03-06 20:24:59 +0000

    Use vendorHash instead of vendorSha256
    Tried to build using Nixpkgs 24.11, but got:
    
        error: buildGoModule: Expect vendorHash instead of vendorSha256
    
    Swapping out that attribute name (and using an appropriate SRI hash) seems to
    work.

--- default.nix
+++ default.nix
@@ -7,5 +7,5 @@ with import nixpkgs { inherit system; };
 buildGoModule {
   name = "nixos-shell";
   src = ./.;
-  vendorSha256 = "0gjj1zn29vyx704y91g77zrs770y2rakksnn9dhg8r6na94njh5a";
+  vendorHash = "sha256-qkBpSVLWZPRgS9bqOVUWHpyj8z/nheQJON3vJOwPUj4=";
 }
